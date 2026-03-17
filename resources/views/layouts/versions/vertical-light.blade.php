9b9c760e5ac49717d145dc0624a47db7289e59f5
MohamedSaeed <mohamedsaeed00451@gmail.com>
2026-03-17 21:14:44 +0200

    update

--- resources/views/layouts/versions/vertical-light.blade.php
+++ resources/views/layouts/versions/vertical-light.blade.php
@@ -1,64 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-
     <meta charset="UTF-8">
     <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="X-UA-Compatible" content="IE=9"/>
 
-    <!-- Title -->
     <title> {{ env('APP_NAME') }} Panel </title>
 
-    <!-- styles -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
     @include('layouts.vertical.styles')
 
 </head>
 
 <body class="main-body app sidebar-mini">
 
-<!-- Loader -->
 <div id="global-loader">
     <img src="{{asset('assets/img/loader.svg')}}" class="loader-img" alt="Loader">
 </div>
-<!-- /Loader -->
-
-<!-- Page -->
 <div class="page">
 
-    <!-- main-sidebar -->
     @include('layouts.vertical.app-sidebar')
 
-    <!-- main-content -->
     <div class='main-content app-content'>
 
-        <!-- main-header -->
         @include('layouts.vertical.main-header')
 
-        <!-- Container open -->
         <div class="container-fluid">
 
             @yield('content')
 
         </div>
-        <!-- Container closed -->
-
     </div>
-    <!-- main-content closed -->
-
-    <!-- footer -->
     @include('layouts.footer')
 
-    <!-- modal -->
     @yield('modal')
 
 </div>
-<!-- Page closed -->
-
-<!-- scripts -->
 @include('layouts.vertical.scripts')
 
-    </body>
-
+</body>
 </html>
-
--- resources/views/layouts/vertical/styles.blade.php
+++ resources/views/layouts/vertical/styles.blade.php
@@ -1,26 +1,21 @@
-<!-- Favicon -->
 <link rel="icon" href="{{asset(getTenantInfo()->image)}}" type="image/x-icon"/>
-<!-- Icons css -->
 <link href="{{asset('assets/plugins/icons/icons.css')}}" rel="stylesheet">
-<!--  Right-sidemenu css -->
 <link href="{{asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
-<!-- P-scroll bar css-->
 <link href="{{asset('assets/plugins/perfect-scrollbar/p-scrollbar.css')}}" rel="stylesheet"/>
 
-@yield('styles')
-
-<!-- Bootstrap css -->
 <link href="{{asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-<!-- Sidemenu css -->
 <link rel="stylesheet" href="{{asset('assets/css/sidemenu.css')}}">
-<!--- Style css --->
 <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
 <link href="{{asset('assets/css/boxed.css')}}" rel="stylesheet">
-<!---Skinmodes css-->
 <link href="{{asset('assets/css/skin-modes.css')}}" rel="stylesheet"/>
-<!--- Animations css-->
 <link href="{{asset('assets/css/animate.css')}}" rel="stylesheet">
 
-<!-- Toastr CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
 
+<style>
+    body, h1, h2, h3, h4, h5, h6, p, a, span, div, input, button, select, textarea, label {
+        font-family: 'Cairo', sans-serif !important;
+    }
+</style>
+
+@yield('styles')
