0f5c514c97079f663348c97fc9641027552cc6e2
MohamedSaeed <mohamedsaeed00451@gmail.com>
2026-04-23 21:44:38 +0200

    update

--- resources/views/pages/items/create.blade.php
+++ resources/views/pages/items/create.blade.php
@@ -832,43 +832,60 @@
                                         </div>
                                     @endforeach
                                 @else
-                                    @php $uid = uniqid(); @endphp
-                                    <div class="row route-row mb-3 align-items-center p-3 rounded"
-                                         style="background-color: #f0fdf4; border: 1px dashed #bbf7d0;">
-                                        <div class="col-md-3 mb-2 mb-md-0">
-                                            <label class="form-label fw-bold">Title (EN)</label>
-                                            <input type="text" name="route_title_en[]" class="form-control"
-                                                   placeholder="e.g. Flight ticket" required>
-                                        </div>
-                                        <div class="col-md-3 mb-2 mb-md-0">
-                                            <label class="form-label fw-bold">Title (AR)</label>
-                                            <input type="text" name="route_title_ar[]" class="form-control"
-                                                   placeholder="مثال: تذاكر الطيران">
-                                        </div>
-                                        <div class="col-md-3 mb-2 mb-md-0">
-                                            <label class="form-label fw-bold">Icon/Image</label>
-                                            <div class="media-selector-group">
-                                                <input type="text" id="route_icon_{{ $uid }}" name="route_icon[]"
-                                                       readonly placeholder="Icon..."
-                                                       onclick="$('#btn_route_{{ $uid }}').click()">
-                                                <button type="button" id="btn_route_{{ $uid }}"
-                                                        class="btn btn-primary btn-choose open-gallery"
-                                                        data-input="route_icon_{{ $uid }}"
-                                                        data-preview="preview_route_{{ $uid }}">Choose
-                                                </button>
+                                    @php
+                                        $defaultIncludes = [
+                                            ['ar' => 'طيران دولي', 'en' => 'International Flight'],
+                                            ['ar' => 'طيران دولي و طيران داخلي', 'en' => 'International and Domestic Flights'],
+                                            ['ar' => 'فنادق ٤ نجوم', 'en' => '4-Star Hotels'],
+                                            ['ar' => 'فنادق ٥ نجوم', 'en' => '5-Star Hotels'],
+                                            ['ar' => 'فنادق ٥ و ٤ نجوم', 'en' => '5 and 4-Star Hotels'],
+                                            ['ar' => 'المواصلات والتنقلات', 'en' => 'Transportation and Transfers'],
+                                            ['ar' => 'الجولات السياحية', 'en' => 'Sightseeing Tours'],
+                                            ['ar' => 'مشرف سياحي', 'en' => 'Tour Escort'],
+                                            ['ar' => 'مرشد سياحي', 'en' => 'Tour Guide'],
+                                            ['ar' => 'خطوط اتصال', 'en' => 'SIM Cards / Communication'],
+                                        ];
+                                    @endphp
+
+                                    @foreach($defaultIncludes as $index => $include)
+                                        @php $uid = uniqid() . $index; @endphp
+                                        <div class="row route-row mb-3 align-items-center p-3 rounded"
+                                             style="background-color: #f0fdf4; border: 1px dashed #bbf7d0;">
+                                            <div class="col-md-3 mb-2 mb-md-0">
+                                                <label class="form-label fw-bold">Title (EN)</label>
+                                                <input type="text" name="route_title_en[]" class="form-control"
+                                                       value="{{ $include['en'] }}" required>
                                             </div>
-                                            <div id="preview_route_{{ $uid }}" class="mt-1"></div>
+                                            <div class="col-md-3 mb-2 mb-md-0">
+                                                <label class="form-label fw-bold">Title (AR)</label>
+                                                <input type="text" name="route_title_ar[]" class="form-control"
+                                                       value="{{ $include['ar'] }}">
+                                            </div>
+                                            <div class="col-md-3 mb-2 mb-md-0">
+                                                <label class="form-label fw-bold">Icon/Image</label>
+                                                <div class="media-selector-group">
+                                                    <input type="text" id="route_icon_{{ $uid }}" name="route_icon[]"
+                                                           readonly placeholder="Icon..."
+                                                           onclick="$('#btn_route_{{ $uid }}').click()">
+                                                    <button type="button" id="btn_route_{{ $uid }}"
+                                                            class="btn btn-primary btn-choose open-gallery"
+                                                            data-input="route_icon_{{ $uid }}"
+                                                            data-preview="preview_route_{{ $uid }}">Choose
+                                                    </button>
+                                                </div>
+                                                <div id="preview_route_{{ $uid }}" class="mt-1"></div>
+                                            </div>
+                                            <div class="col-md-2 mb-2 mb-md-0">
+                                                <label class="form-label fw-bold">Order (الترتيب)</label>
+                                                <input type="number" name="route_order[]" class="form-control" value="{{ $index + 1 }}">
+                                            </div>
+                                            <div class="col-md-1 mt-4 text-center">
+                                                <button type="button"
+                                                        class="btn btn-danger remove-route-row w-100 shadow-sm"
+                                                        title="Remove"><i class="fas fa-times"></i></button>
+                                            </div>
                                         </div>
-                                        <div class="col-md-2 mb-2 mb-md-0">
-                                            <label class="form-label fw-bold">Order (الترتيب)</label>
-                                            <input type="number" name="route_order[]" class="form-control" value="0">
-                                        </div>
-                                        <div class="col-md-1 mt-4 text-center">
-                                            <button type="button"
-                                                    class="btn btn-danger remove-route-row w-100 shadow-sm"
-                                                    title="Remove"><i class="fas fa-times"></i></button>
-                                        </div>
-                                    </div>
+                                    @endforeach
                                 @endif
                             </div>
 
